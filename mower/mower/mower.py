3eb2f576f67f54e934bbfd4f0cb21391e7cc1207
Mart Coppelmans <mart.coppelmans@gmail.com>
2023-05-19 18:30:25 +0200

    saving map

--- mower/mower/mower.py
+++ mower/mower/mower.py
@@ -2,13 +2,14 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry, OccupancyGrid
 from geometry_msgs.msg import Polygon
+from PIL import Image
 
 class Mower(Node):
     def __init__(self):
         super().__init__('mower')
         self.get_logger().info('Hi from mower.')
         self.odom_callback = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
-        self.map_publisher = self.create_publisher(OccupancyGrid, 'map', 10)
+        self.map_publisher = self.create_publisher(OccupancyGrid, 'map', 1)
         self.max_x = 0
         self.max_y = 0
         self.min_x = 0
@@ -31,7 +32,7 @@ class Mower(Node):
         map = OccupancyGrid()
         map.header.frame_id = 'map'
         map.header.stamp = self.get_clock().now().to_msg()
-        map.info.resolution = 0.1
+        map.info.resolution = 0.05
         map.info.width = int((self.max_x - self.min_x) / map.info.resolution) + 10
         map.info.height = int((self.max_y - self.min_y) / map.info.resolution) +10
         map.info.origin.position.x = 0.0
@@ -42,17 +43,35 @@ class Mower(Node):
         map.info.origin.orientation.z = 0.0
         map.info.origin.orientation.w = 1.0
 
+        img = Image.new('L', (map.info.width, map.info.height))
         for i in range(map.info.width * map.info.height):
             map.data.append(-1)
+            img.putpixel((i % map.info.width, i // map.info.width), 255)
+        
+
 
         for point in self.polygon.points:
             grid_x = int((point.x - self.min_x)/ map.info.resolution)
             grid_y = int((point.y - self.min_y)/ map.info.resolution)
             index = grid_y * map.info.width + grid_x
             map.data[index] = 100  # Assuming 100 represents occupied
+            img.putpixel((grid_x, grid_y), 0)
 
         self.map_publisher.publish(map)
-        
+        img.save('map.png')
+
+        #create and save map.yaml
+        with open('map.yaml', 'w') as f:
+            f.write('image: map.png\n')
+            f.write('resolution: {}\n'.format(map.info.resolution))
+            f.write('origin: [{}, {}, 0.0]\n'.format(map.info.origin.position.x, map.info.origin.position.y))
+            f.write('negate: 0\n')
+            f.write('occupied_thresh: 0.65\n')
+            f.write('free_thresh: 0.196\n')
+
+
+            
+    
 
 def main():
     rclpy.init()
